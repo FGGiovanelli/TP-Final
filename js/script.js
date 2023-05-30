1152a4509675d66532e7a1309f44c9e1a62a2215
Fernando Giovanelli <fgg.giovanelli@gmail.com>
2023-05-30 10:14:11 -0300

    Se agrega el Proyecto

---
+++ js/script.js
@@ -0,0 +1,68 @@
+
+window.onload = function() {
+  var respuesta = confirm("Atencion!!!Algunos de los precios estan en dolares");
+
+  if (respuesta) {
+      alert ("Perfecto!! Igual no te hacen falta el precio esta al dolar oficial");
+  } else {
+      alert("No te preocupes, podes pagarlos al dolar oficial");
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let destinosMundo = document.querySelector("#destinosMundo");
+
+const getDestinosMundo = async () => {
+  try {
+    const resp = await fetch ("destinosMundo.json");
+    const data = await resp.json();
+
+    console.log(data);
+
+    data.forEach(item => {
+      const crewData = document.createElement("div");
+
+      crewData.innerHTML = `
+        <img src="${item.img}" alt="">
+        <h6>${item.lugar}</h6>
+        <p>${item.descripcion}</p>
+      `;
+
+      destinosMundo.appendChild(crewData);
+    });
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+getDestinosMundo();
